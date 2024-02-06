ecd268e27ec1786a3493e1eaff5742931f12f025
ManuhaAdlin <adlinmanuha2316@gmail.com>
2024-02-06 14:04:13 +0530

    first edit

---
+++ stringreverse.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class stringreverse
+ {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string");
+        String s1 = sc.nextLine();
+        String reverse="";
+        for(int i=0;i<s1.length();i++)
+        {
+            reverse = s1.charAt(i)+reverse;
+        }
+        System.out.println("the reversed string is : "+reverse);
+    }
+}
