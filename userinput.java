4ef158207f4d1e76903a7b4f0e1e222d76960494
ManuhaAdlin <adlinmanuha2316@gmail.com>
2024-02-06 13:36:10 +0530

    first commit

---
+++ userinput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class userinput
+{
+    public static void main(String args [])
+    {
+    
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a value : ");
+        int a = sc.nextInt();
+        System.out.println("enter b value : ");
+        int b = sc.nextInt();
+        int x = a+b;
+        System.out.println("sum of two numbers = " +x);
+    }
+}
