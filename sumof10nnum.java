4312a866f020e29153d94beb9b753cbc5398379d
ManuhaAdlin <adlinmanuha2316@gmail.com>
2024-02-08 22:57:29 +0530

    added

---
+++ sumof10nnum.java
@@ -0,0 +1,12 @@
+public class sumof10nnum
+{
+    public static void main (String[] args)
+    {
+        int sum=0;
+        for(int i=1;i<=10;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
