87f1b4e301e3edf0b9717e0204d291b6a1b8a013
ManuhaAdlin <159104036+ManuhaAdlin@users.noreply.github.com>
2024-02-09 06:23:59 +0530

    Update userinput.java

--- userinput.java
+++ userinput.java
@@ -3,7 +3,6 @@ public class userinput
 {
     public static void main(String args [])
     {
-    
         Scanner sc=new Scanner(System.in);
         System.out.println("enter a value : ");
         int a = sc.nextInt();
@@ -11,5 +10,16 @@ public class userinput
         int b = sc.nextInt();
         int x = a+b;
         System.out.println("sum of two numbers = " +x);
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter the sides of triangle");
+        int s1 = a.nextInt();
+        int s2 = a.nextInt();
+        int s3 = a.nextInt();
+        if(s1==s2 && s2==s3)
+            System.out.println("It is an Equilateral Triangle");
+        else if( s1==s2 || s2==s3 || s3==s1 )
+            System.out.println("It is an Isosceles Triangle");
+        else
+            System.out.println("It is a Scalene Triangle");
     }
 }
